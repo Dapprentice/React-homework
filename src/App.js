0218fc04c6847b5c677a28a9a50da55337a3398f
Oz <jjmgone@hotmail.com>
2022-01-21 20:14:57 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+let sum = 0;
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>
+          Coin Exchange {sum}
+        </h2>
+      </header>
+    </div>
+
+);export default App;
+  }
+  
